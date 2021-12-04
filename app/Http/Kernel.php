33e7bba106d58332778095d5e4a09d4784ec2550
Eduardo Jimenez <10002@iespuertasdelcampo.es>
2021-12-04 16:44:12 +0100

    Creación AdminMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'request_logger' => \App\Http\Middleware\LogRequests::class,
         'auth.jwt' => \App\Http\Middleware\JwtMiddleware::class,
+        'auth.admin' => \App\Http\Middleware\AdminMiddleware::class,
     ];
 }
